653126b200d8d7b293d421f52b9fd5c7271280ae
SungYon <coyking77@gmail.com>
2015-07-28 21:15:28 +0900

    Create VowelCount.rb

---
+++ VowelCount.rb
@@ -0,0 +1,14 @@
+def VowelCount(str)
+
+  # code goes here
+   str = str.scan(/[aeoui]/).count
+   
+    
+  
+  return str
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+VowelCount(STDIN.gets)           
