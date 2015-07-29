5db3f4c9efc47a43ee3f5619737f9e997c1d43e1
SungYon <coyking77@gmail.com>
2015-07-29 19:42:53 +0900

    Create MultiplicativePersistence.rb

---
+++ MultiplicativePersistence.rb
@@ -0,0 +1,26 @@
+def MultiplicativePersistence(num)
+
+  # code goes here
+  count=0
+  value=true
+  while value
+    if num > 9
+      count+=1
+      value=true
+    else
+      break
+    end
+    h=[]
+    num=num.to_s.split("")
+	for k in 0...num.length
+      h.push(num[k].to_i)
+    end
+    num=h.inject(:*)
+  end
+  return count
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+MultiplicativePersistence(STDIN.gets)           
